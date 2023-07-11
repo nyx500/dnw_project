5a30c0d7757d0d18fcce71ea7f67a2b2164dbf43
Ophelia <ophelia.lavey2@gmail.com>
2023-07-11 13:38:55 +0200

    Added table-creation code to index.js

--- index.js
+++ index.js
@@ -10,23 +10,69 @@ const Joi = require('joi');
 
 
 // handles post requests --> must define it before defining the routes or doesn't work!!!
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//items in the global namespace are accessible throught out the node application
-global.db = new sqlite3.Database('./database.db',function(err){
-  if(err){
+// items in the global namespace are accessible throught out the node application
+global.db = new sqlite3.Database('./database.db', function (err) {
+  if (err) {
     console.error(err);
     process.exit(1); //Bail out we can't connect to the DB
-  }else{
+  } else {
     console.log("Database connected");
     global.db.run("PRAGMA foreign_keys=ON"); //This tells SQLite to pay attention to foreign key constraints
-    // SQLite Query: creates the 'comments' table if does not already exist
-    var create_comments_table = `CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY,`
-        + `comment VARCHAR(1000) NOT NULL, article_id INTEGER NOT NULL,`
-        + `FOREIGN KEY (article_id) REFERENCES articles (id)`
-        + `);`;
-    global.db.run(create_comments_table);
+
+    // Creates the 'blog' table in the DB if and only if the table does not exist yet:
+    var create_blog_table_query = `CREATE TABLE IF NOT EXISTS blog (id INTEGER PRIMARY KEY,`
+      + `title VARCHAR(255) DEFAULT  "My Blog" NOT NULL, ` // Sets default title to 'My Blog'
+      + `subtitle VARCHAR(500) DEFAULT "Welcome to my blog!",` // Sets default subtitle
+      + `author VARCHAR(255) DEFAULT "Blog Author" NOT NULL,` // Sets default author
+      + `datetime_created DATETIME DEFAULT CURRENT_TIMESTAMP);`; // Sets default blog-creation time to now (GMT)
   }
+  
+  global.db.run(create_blog_table_query, function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(1); //Bail out if we can't create 'blog' table
+    } else {
+      console.log("Success: 'blog' table exists in db");
+
+      // Creates the 'articles' table in the DB if and only if the table does not exist yet:
+      var create_articles_table_query = `CREATE TABLE IF NOT EXISTS articles `
+        // Sets current time in GMT as default for article created time
+        + `(id INTEGER PRIMARY KEY, datetime_created DATETIME DEFAULT CURRENT_TIMESTAMP,` 
+        + `datetime_modified DATETIME DEFAULT CURRENT_TIMESTAMP,` // Last modified field also set to curr time
+        + `datetime_published DATETIME, title VARCHAR(500) DEFAULT "My Article" NOT NULL,`
+        + `subtitle VARCHAR(500) DEFAULT "My Subtitle" NOT NULL,`
+        + `content TEXT DEFAULT "" NOT NULL, is_published BOOLEAN DEFAULT 0,` // Sets default content as empty str
+        + `likes INTEGER DEFAULT 0);`; // Sets default number of likes to 0
+
+      global.db.run(create_articles_table_query, function (err) {
+        if (err) {
+          console.log(err);
+          process.exit(1); // Bail out we can't create 'articles' table
+        }
+        else {
+          console.log("Success: 'articles' table exists in db");
+
+          // Create the 'comments' table if it does not already exist
+          var create_comments_table_query = `CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY,`
+            + `comment VARCHAR(1000) NOT NULL, article_id INTEGER NOT NULL,`
+             // Establishes relationship with article using Foreign Key
+            + `FOREIGN KEY (article_id) REFERENCES articles (id)`
+            + `);`;
+            
+          global.db.run(create_comments_table_query, function (err) {
+            if (err) {
+              console.log(err);
+              process.exit(1);
+            } else {
+              console.log("Success: 'comments' table exists in db");
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 const readerRoutes = require('./routes/reader');
@@ -36,7 +82,7 @@ const authorRoutes = require('./routes/author');
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 //set the app to use ejs for rendering
-app.set("views",__dirname + "/views");  
+app.set("views", __dirname + "/views");
 app.set('view engine', 'ejs');
 
 // test page
--- routes/author.js
+++ routes/author.js
@@ -13,99 +13,43 @@ const { check, validationResult } = require('express-validator');
 
 // Author Home Page Route: the author can create, review, and edit articles here
 router.get("/", (req, res) => {
-
-    // SQLite Query: creates the 'blog' table in the DB if and only if the table does not exist yet.
-    var create_blog_table_query = `CREATE TABLE IF NOT EXISTS blog (id INTEGER PRIMARY KEY,`
-        + `title VARCHAR(255) DEFAULT  "My Blog" NOT NULL, ` // Set default title (can be changed in Settings)
-        + `subtitle VARCHAR(500) DEFAULT "Welcome to my blog!",` // Set default subtitle
-        + `author VARCHAR(255) DEFAULT "Blog Author" NOT NULL,` // Set defaut author
-        + `datetime_created DATETIME DEFAULT CURRENT_TIMESTAMP);`; // Set default blog-creation time to now in GMT
-
-    // Run the blog-table creating query...
-    db.run(create_blog_table_query, function (err) {
+    /** Retrieves title,subtitle,author values from the
+    *  single row in the table which stores details about this blog */
+    var get_blog_data_query = `SELECT title, subtitle, author FROM blog;`;
+    db.get(get_blog_data_query, function (err, blog_data){
         if (err) {
-            console.log("Error - could not create blog table: " + err);
-            // Exit program if could not create the table
+            console.error(err);
             process.exit(1);
         } else {
-            /** If the 'blog' table has been created, retrieve title,subtitle,author values from the
-             *  single row in the table which stores details about this blog */
-            var get_blog_data_query = `SELECT title, subtitle, author FROM blog;`;
-            db.get(get_blog_data_query,
-                function (err, blog_data) {
+            // No blog data exists yet --> create the single entry required for the blog site to work (storing blog's settings)
+            if (blog_data === undefined){
+                 // Creates a new blog (1 row) in 'blog' table with title/subtitle/author, set to the default vals for title/subtitle etc.
+                 var create_blog_query = `INSERT INTO blog DEFAULT VALUES;`;
+                 db.run(create_blog_query, function (err) {
+                     if (err) {
+                         console.log("Error - could not create blog!: " + err);
+                         process.exit(1);
+                     } else {
+                        // Redirect to same route once blog data is succesfully created
+                        res.redirect("/author/");
+                     }
+                 });
+            }  else { // If blog_data exists and has been fetched from DB:
+
+                 // Now get all the author's articles from 'articles' table in DB
+                 var retrieve_articles_query = `SELECT * FROM articles;`;
+                 db.all(retrieve_articles_query, function(err, articles_data){
                     if (err) {
-                        console.error("Error: " + err);
+                        console.log("Cannot retrieve articles from database: " + err);
                         process.exit(1);
                     } else {
-                        // No blog exists yet --> create the single blog row required for the blog site to work
-                        if (blog_data === undefined) {
-                            // Creates a new blog (1 row) in 'blog' table with title/subtitle/author all set to the defaults
-                            var create_blog_query = `INSERT INTO blog DEFAULT VALUES`;
-                            db.run(create_blog_query, function (err) {
-                                if (err) {
-                                    console.log("Error - could not create blog!: " + err);
-                                    // Exit program if could not create the blog data
-                                    process.exit(1);
-                                } else {
-                                    /** Redirects to this route again once blog is created:
-                                     *  first row of 'blog' table storing title/subtitle should now be fetched from the DB!
-                                    */
-                                    res.redirect("/author/");
-                                }
-                            })
-                        } else {
-
-                            // Query which checks if the 'articles' table exists already:
-                            var check_articles_table_exists_query = `SELECT * FROM sqlite_master`
-                                + ` WHERE type='table' AND name='articles';`;
-                            db.get(check_articles_table_exists_query, function (err, articles_data) {
-                                if (err) {
-                                    process.exit(1);
-                                } else {
-                                    // If undefined, then 'articles' table has not been created --> create it
-                                    if (articles_data === undefined) {
-                                        // Create the 'articles' table as it does not already exist, set default values
-                                        var create_articles_table_query = `CREATE TABLE IF NOT EXISTS articles `
-                                            + `(id INTEGER PRIMARY KEY, datetime_created DATETIME DEFAULT CURRENT_TIMESTAMP,`
-                                            + `datetime_modified DATETIME DEFAULT CURRENT_TIMESTAMP,`
-                                            + `datetime_published DATETIME, title VARCHAR(500) DEFAULT "Untitled Article" NOT NULL,`
-                                            + `subtitle VARCHAR(500) DEFAULT "Undefined Subtitle" NOT NULL,`
-                                            + `content TEXT DEFAULT "" NOT NULL, is_published BOOLEAN DEFAULT 0,`
-                                            + `likes INTEGER DEFAULT 0);`;
-
-                                        // Creates the articles table
-                                        db.run(create_articles_table_query, function (err) {
-                                            if (err) {
-                                                console.log("Error - cannot create articles table! " + err);
-                                                // Exit program if could not create the table
-                                                process.exit(1);
-                                            } else {
-                                                // Redirects to this route again once articles table is created!
-                                                res.redirect("/author/");
-                                            }
-                                        });
-                                    } else {
-                                        /** Run this code only if article and blog table exist:
-                                         * Retrieves all the articles from the 'articles' table in DB
-                                        */
-                                        db.all(`SELECT * FROM articles;`, function (err, articles_data) {
-                                            if (err) {
-                                                console.log("Articles error: " + err);
-                                                process.exit(1);
-                                            } else {
-                                                console.log(articles_data.length);
-                                                res.render("author/author-home-page", {
-                                                    blog: blog_data,
-                                                    articles: articles_data
-                                                });
-                                            }
-                                        });
-                                    }
-                                }
-                            });
-                        }
+                        res.render("author/author-home-page", {
+                            blog: blog_data,
+                            articles: articles_data
+                        });
                     }
-                });
+                 });
+            }
         }
     });
 });
@@ -284,7 +228,7 @@ router.post("/edit-article", articleValidate, (req, res) => {
             })
         );
     }
-    // Data is valid --> update the DB to store new blog settings
+    // Data is valid --> update the DB to store new article data
     else {
             // // Update the article and last-modified date in the DB
             var update_query = `UPDATE articles SET datetime_modified = CURRENT_TIMESTAMP,`
@@ -294,7 +238,7 @@ router.post("/edit-article", articleValidate, (req, res) => {
                 if (err) {
                     console.log("ERROR - could not publish article! " + err);
                 } else {
-                    console.log("Success - updated the article.");
+                    console.log("Success: updated the article.");
                     res.redirect("/author/");
                 }
             });
