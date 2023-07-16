5f08edce3fd292e894ef00aeaa59fff3bbf3b92b
Ophelia <ophelia.lavey2@gmail.com>
2023-07-17 00:25:26 +0200

    done

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 
 
-/** Functionality to scroll to the errors container on the Comments form on the View Article page if errors exist
+/** Function to scroll to the errors container on the Comments form on the View Article page if errors exist
  * Ref - instructions how to scroll smoothly to element:
  * https://stackoverflow.com/questions/49820013/javascript-scrollintoview-smooth-scroll-and-offset
 */
@@ -17,31 +17,34 @@ function scrollToErrors(element) {
 
 // Execute code only when 'body' tag is loaded
 document.querySelector('body').onload = function() {
-
-    console.log("Loaded");
     
+    // Share button functionality: add alert when click event happens for each 'share' button on each published article Author's home page
     [...document.getElementsByClassName('share-button')].forEach((button) => {
         button.addEventListener("click", function() {
+            // Share button ID is always 'share-button-<article-id>', so split this string and get the id from the end of the string
             let sharingLink = `${window.location.origin}/reader/article?id=${button.id.split("share-button-")[1]}`;
             alert("Sharing Link: " + sharingLink);
         });
     });
 
+    // Functionality to scroll to errors if they exist after a POST request
     var errorsContainer = document.querySelector('#errors-container');
     if (errorsContainer)
     {
         scrollToErrors(errorsContainer);
     }
+
     /** Functionality to toggle menu on smaller screens when menu button is clicked
      * Ref: https://www.devwares.com/blog/how-to-create-a-beautiful-responsive-navbar-using-tailwind-css/
     */
     const menuButton = document.querySelector('button.menu-button');
     const menu = document.querySelector('.hidden-menu');
     const nav = document.querySelector('nav');
-    menuButton.addEventListener('click', () => {
-        menu.classList.toggle('hidden');
-        nav.classList.toggle('py-0');
-    });
-
+    if (menuButton){
+        menuButton.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+            nav.classList.toggle('py-0');
+        });
+    }
 };
 
