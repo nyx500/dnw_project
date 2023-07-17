8220b4b7978d8380065c289caf83e41b094aeb2f
Ophelia <ophelia.lavey2@gmail.com>
2023-07-17 13:46:06 +0200

    Finished adding comments to script.js about dark mode functions

--- public/script.js
+++ public/script.js
@@ -1,69 +1,4 @@
 
-
-/** Function to scroll to the errors container on the Comments form on the View Article page if errors exist
- * Ref - instructions how to scroll smoothly to element:
- * https://stackoverflow.com/questions/49820013/javascript-scrollintoview-smooth-scroll-and-offset
-*/
-function scrollToErrors(element) {
-    var offset = 200;
-    var elementPosition = element.getBoundingClientRect().top;
-    console.log(element.getBoundingClientRect());
-    var offsetPosition = elementPosition + window.scrollY - offset;
-    window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth"
-   });
-}
-
-/* Dark mode functionality */
-function toggleDarkMode() {
-    var button = document.querySelector('#darkmode-button');
-    // Toggles temporary dark mode session variable when click on the Dark Mode buton found on every page
-    button.addEventListener("click", function() {
-        // Get the object which stores if dark mode is off/on from sessionStorage
-        var darkVariable = sessionStorage.getItem('dark');
-        if (darkVariable === undefined || darkVariable === null || darkVariable === 'off')
-        {   
-            sessionStorage.setItem('dark', 'on');
-            document.documentElement.classList.add("dark");
-            button.innerHTML = "Light Mode";
-            console.log("dark mode is on");
-            document.querySelector('#menu-icon').style.stroke = "lightgrey";
-        }
-        else
-        {   
-            sessionStorage.setItem('dark', 'off');
-            document.documentElement.classList.remove("dark");
-            button.innerHTML = "Dark Mode";
-            console.log("dark mode is off");
-            document.querySelector('#menu-icon').style.stroke = "black";
-        }
-    });
-}
-
-// uses the value assigned to the 'dark' item in session storage to add/remove the 'dark' class from html (root element) when page loads
-function applyDarkModeToHTML() {
-    var darkVariable = sessionStorage.getItem('dark');
-    if (darkVariable === 'on')
-    {   
-        document.querySelector('#darkmode-button').innerHTML = "Light Mode";
-        if (!document.documentElement.classList.contains('dark'))
-        {
-            document.documentElement.classList.add("dark");
-        }
-        document.querySelector('#menu-icon').style.stroke = "lightgrey";
-        console.log("pg loaded: dark mode is on");
-    } else {
-        document.querySelector('#darkmode-button').innerHTML = "Dark Mode";
-        if (document.documentElement.classList.contains('dark'))
-        {
-            document.documentElement.classList.remove("dark");
-        } 
-        document.querySelector('#menu-icon').style.stroke = "black";
-        console.log("pg loaded: dark mode is off");
-    }
-}
-
 // Execute code only when 'body' tag is loaded
 document.querySelector('body').onload = function() {
     
@@ -96,9 +31,79 @@ document.querySelector('body').onload = function() {
         });
     }
 
-    // Apply dark mode if selected when the page loads
+    // Applies dark mode if it is selected in sessionStorage when the page loads
     applyDarkModeToHTML();
-    // Toggle the dark mode by clicking on 'Dark Mode' button
+    // Allows toggling of the dark mode by clicking on 'Dark Mode' button
     toggleDarkMode();
 };
 
+/////////////////////////////////////////////////////////////////HELPER FUNCTIONS////////////////////////////////////////////////////////////////////////////////////////
+/** Function to scroll to the errors container on the Comments form on the View Article page if errors exist
+ * Ref - instructions how to scroll smoothly to element:
+ * https://stackoverflow.com/questions/49820013/javascript-scrollintoview-smooth-scroll-and-offset
+*/
+function scrollToErrors(element) {
+    var offset = 200;
+    var elementPosition = element.getBoundingClientRect().top;
+    console.log(element.getBoundingClientRect());
+    var offsetPosition = elementPosition + window.scrollY - offset;
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+   });
+}
+
+/* Dark mode functionality */
+function toggleDarkMode() {
+    // Select and store dark mode button from document
+    var button = document.querySelector('#darkmode-button');
+    // Toggles the temporary dark mode session variable when click event on the Dark Mode button
+    button.addEventListener("click", function() {
+        // Select the object which stores if dark mode is off/on from sessionStorage
+        var darkVariable = sessionStorage.getItem('dark');
+        // To turn dark mode on if it is not:
+        if (darkVariable === undefined || darkVariable === null || darkVariable === 'off')
+        {   
+            // Update 'dark' session variable to 'on'
+            sessionStorage.setItem('dark', 'on');
+            // Add the 'dark' class to the DOM's 'html' element
+            document.documentElement.classList.add("dark");
+            // Change the text inside the Dark Mode button to 'Light Mode'
+            button.innerHTML = "Light Mode";
+            // Change the color of the SVG menu icon in the navbar on smaller screens (not accessible with Tailwind!) from black to light grey
+            document.querySelector('#menu-icon').style.stroke = "lightgrey";
+        }
+        // To turn dark mode off:
+        else
+        {   
+            sessionStorage.setItem('dark', 'off');
+            document.documentElement.classList.remove("dark");
+            button.innerHTML = "Dark Mode";
+            document.querySelector('#menu-icon').style.stroke = "black";
+        }
+    });
+}
+
+// When the page loads, check if sessionStorage has recorded dark mode as on: if it is on, add 'dark' to <html>'s class list
+function applyDarkModeToHTML() {
+    var darkVariable = sessionStorage.getItem('dark');
+    if (darkVariable === 'on')
+    {   
+        document.querySelector('#darkmode-button').innerHTML = "Light Mode";
+        // Check if the DOM's <html> element contains the 'dark' class: if not, then add it to the class list
+        if (!document.documentElement.classList.contains('dark'))
+        {
+            document.documentElement.classList.add("dark");
+        }
+        // Change menu icon to light grey on dark screens (as it is on a black navbar)
+        document.querySelector('#menu-icon').style.stroke = "lightgrey";
+    } else {
+        document.querySelector('#darkmode-button').innerHTML = "Dark Mode";
+        if (document.documentElement.classList.contains('dark'))
+        {
+            document.documentElement.classList.remove("dark");
+        } 
+        // Change menu icon back to black on light mode
+        document.querySelector('#menu-icon').style.stroke = "black";
+    }
+}
